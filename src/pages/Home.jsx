77f2e5b67b4ceecbad5d34b567192ef3f17a164a
Leonel Aguirre <leonel.aguirre77@gmail.com>
2023-05-09 19:03:57 -0600

    :lipstick: Home page ui implemented.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
+import "./Home.scss"
+
 import React from "react"
 import { useRouter } from "next/router"
 import { useDispatch } from "react-redux"
 
 import { fetchAuthorizationURL } from "@/redux/actions/authenticationActions"
+import Button from "@/components/Button/Button"
 
 const Home = () => {
   const dispatch = useDispatch()
@@ -18,8 +21,30 @@ const Home = () => {
 
   return (
     <div className="home">
-      <h1>TEST</h1>
-      <button onClick={loginButtonHandler}>Login</button>
+      <div className="home__content-wrapper">
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src="/static/images/logo.svg" className="home__logo" alt="Logo" />
+        <div className="home__texts-container">
+          <h1 className="home__tag-line">
+            Visualize Your Music, Hear Your Data
+          </h1>
+          <p className="home__description">
+            Audiograph visualizes your music and listening habits, allowing you
+            to easily track your top genres, artists, and tracks over time and
+            compare your activity to others.
+          </p>
+        </div>
+        <div className="home__sign-in-cta-container">
+          <Button
+            className="home__sign-in-cta"
+            onClick={loginButtonHandler}
+            isSmall={true}
+            type={Button.SUCCESS}
+          >
+            Sign In With Your Spotify Account
+          </Button>
+        </div>
+      </div>
     </div>
   )
 }
