af587ea681d1ca62dc825751b667653b2b6ee37e
Leonel Aguirre <leonel.aguirre77@gmail.com>
2023-05-10 19:03:09 -0600

    :bug: Prevent icons flash.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,9 +2,14 @@ import "./App.scss"
 
 import { Provider } from "react-redux"
 import { configureStore } from "@reduxjs/toolkit"
+import "@fortawesome/fontawesome-svg-core/styles.css"
+import { config } from "@fortawesome/fontawesome-svg-core"
 
 import reducer from "@/redux/reducer"
 import AfterLoginLayout from "@/layouts/AfterLoginLayout"
+
+// Prevent fontawesome from dynamically adding its css since we are going to include it manually
+config.autoAddCss = false
 
 const store = configureStore({
   reducer,
