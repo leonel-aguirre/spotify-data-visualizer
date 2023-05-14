8f4e832e9c3950b91478f9b5fbb2adaeef78bedd
Leonel Aguirre <leonel.aguirre77@gmail.com>
2023-05-14 17:21:17 -0600

    :sparkles: User tops status handling added to reducer.

--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -1,15 +1,34 @@
 export const SET_USER_DATA = "SET_USER_DATA"
+export const SET_TOPS_STATUS = "SET_TOPS_STATUS"
 
 export const defaultState = {
-  user: {},
+  user: {
+    userEmail: "",
+    userID: "",
+    userImageURL: "",
+    userName: "",
+  },
+  topsStatus: {
+    artistShortTerm: false,
+    artistMidTerm: false,
+    artistLongTerm: false,
+    trackShortTerm: false,
+    trackMidTerm: false,
+    trackLongTerm: false,
+  },
 }
 
 const reducer = (state = defaultState, action) => {
   switch (action.type) {
     case SET_USER_DATA:
       return {
-        ...defaultState,
+        ...state,
         user: { ...action.payload.data },
+      }
+    case SET_TOPS_STATUS:
+      return {
+        ...state,
+        topsStatus: { ...action.payload.data },
       }
     default:
       return state
@@ -19,3 +38,4 @@ const reducer = (state = defaultState, action) => {
 export default reducer
 
 export const selectUser = (state) => state.userReducer.user
+export const selectTopsStatus = (state) => state.userReducer.topsStatus
